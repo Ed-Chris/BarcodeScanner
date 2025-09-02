3d209a9e3e2273dd9b545a975b2a5962b4c5f18f
Ed-Chris <yedukrishnan215@gmail.com>
2025-09-01 20:25:32 -0600

    Changing

--- notify.py
+++ notify.py
@@ -3,8 +3,8 @@ from datetime import datetime, timedelta
 import smtplib
 from email.message import EmailMessage
 from config import db_connect
-# from email_secrets import EMAIL_ADDRESS, EMAIL_PASSWORD, RECIPIENTS
 import os
+from dotenv import load_dotenv
 
 conn = db_connect()
 c = conn.cursor()
@@ -16,7 +16,7 @@ c.execute("SELECT product_name, expiry_date FROM products")
 rows = c.fetchall()
 
 expiring_soon = [r for r in rows if datetime.strptime(r[1], "%Y-%m-%d").date() <= threshold]
-
+load_dotenv()
 EMAIL_ADDRESS = os.environ['EMAIL_ADDRESS']
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 RECIPIENTS = os.environ['RECIPIENTS'].split(',')  # comma-separated list
@@ -29,8 +29,11 @@ if expiring_soon:
     body = "\n".join([f"{r[0]} expires on {r[1]}" for r in expiring_soon])
     msg.set_content(body)
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        server.send_message(msg)
-        print("Notification email sent.")
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            server.send_message(msg)
+        print("Email sent successfully!")
+    except Exception as e:
+        print("Error sending email:", e)
