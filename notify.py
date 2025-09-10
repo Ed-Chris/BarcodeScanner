86a652987c58edbf48a68bda98d26ef17c650307
Ed-Chris <yedukrishnan215@gmail.com>
2025-09-10 01:05:34 -0600

    Changing the emiail updates and adding the csv file

--- notify.py
+++ notify.py
@@ -5,6 +5,7 @@ from config import db_run_query
 import os
 import pandas as pd
 from dotenv import load_dotenv
+import io
 
 # Load env variables
 load_dotenv()
@@ -13,29 +14,46 @@ EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 RECIPIENTS = os.environ['RECIPIENTS'].split(',')  # comma-separated list
 
 # Query the database ---
-df = db_run_query("SELECT product_name, expiry_date FROM products")
+df = db_run_query("SELECT product_name, expiry_date FROM products;")
 
-# Process data ---
+# Process data
 today = datetime.today().date()
+tomorrow = today + timedelta(days=1)
 threshold = today + timedelta(days=3)
 
 # Ensure expiry_date is a date
 df["expiry_date"] = pd.to_datetime(df["expiry_date"]).dt.date
 
-# Filter
-expiring_soon = df[df["expiry_date"] <= threshold]
+# Filter between tomorrow and threshold
+expiring_soon = df[(df["expiry_date"] >= tomorrow) & (df["expiry_date"] <= threshold)]
+
+# --- Create CSV directly in memory ---
+csv_buffer = io.StringIO()
+expiring_soon.to_csv(csv_buffer, index=False)
+csv_bytes = csv_buffer.getvalue().encode("utf-8")
 
 # Send email if needed ---
 if not expiring_soon.empty:
+    tomorrow = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
     msg = EmailMessage()
-    msg['Subject'] = "Products Expiring Soon!"
+    msg["Subject"] = f"📦 Products Expiring soon! {tomorrow}"
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = ", ".join(RECIPIENTS)
 
-    body = "\n".join([f"{row.product_name} expires on {row.expiry_date}"
-                      for row in expiring_soon.itertuples()])
+    body = "Hi,\n\nPlease find attached the list of products expiring in the next 3 days:\n\n"
+    body += "\n".join([f"- {row.product_name} expires on {row.expiry_date}"
+                    for row in expiring_soon.itertuples()])
+    body += "\n\nPlease take necessary action."
+    body += "\n\nRegards,\nExpiry Tracker"
     msg.set_content(body)
 
+    # --- Attach to email ---
+    msg.add_attachment(
+        csv_bytes,
+        maintype="text",
+        subtype="csv",
+        filename="expiring_products.csv"
+)
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
